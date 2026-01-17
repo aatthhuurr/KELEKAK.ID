b54d3ed42eaab9b66677fade573d7b845fe1d2f9
aatthhuurr <2411500025@mahasiswa.atmaluhur.ac.id>
2026-01-17 14:30:48 +0700

    Tambah fungsi tambah dan simpan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Saya daftarin jalan buat nampilin form tambah data
+$routes->get('tambah', 'Home::tambah');
+
+// Saya daftarin jalan buat proses nyimpen datanya (pakai post karena kirim data)
+$routes->post('simpan', 'Home::simpan');
