3f9d5ebbf5ad89173abd810976680be23c6f4883
aatthhuurr <2411500025@mahasiswa.atmaluhur.ac.id>
2026-01-17 14:16:47 +0700

    add:

--- app/Models/BahasaModel.php
+++ app/Models/BahasaModel.php
@@ -9,7 +9,7 @@ class BahasaModel extends Model
 {
     // 1. Kasih tau CI4 tabel mana yang mau kita obok-obok
     protected $table            = 'bahasa';
-    
+
     // 2. Kasih tau kunci utamanya (Primary Key) yang mana
     protected $primaryKey       = 'id_bahasa';
 
@@ -22,13 +22,13 @@ class BahasaModel extends Model
     // 5. INI PENTING: Daftar kolom yang BOLEH diisi lewat aplikasi. 
     // Kalau gak didaftarin di sini, datanya gak bakal masuk ke database (fitur keamanan).
     protected $allowedFields    = [
-        'kategori', 
-        'kata_indonesia', 
-        'kata_inggris', 
-        'kata_daerah', 
-        'dialek_wilayah', 
-        'filosofi', 
-        'file_audio', 
+        'kategori',
+        'kata_indonesia',
+        'kata_inggris',
+        'kata_daerah',
+        'dialek_wilayah',
+        'filosofi',
+        'file_audio',
         'file_gambar'
     ];
 
