a9b80b5223d68f75b5871d0f86ffc6884273202b
aatthhuurr <2411500025@mahasiswa.atmaluhur.ac.id>
2026-01-17 14:16:07 +0700

    Tambah untuk BahasaModel

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,26 @@
 
 namespace App\Controllers;
 
+// panggil Model Bahasa yang tadi sudah dibuat biar bisa dipakai di sini
+use App\Models\BahasaModel;
+
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        // 1. buat objek baru dari BahasaModel (Panggil kurirnya)
+        $bahasaModel = new BahasaModel();
+
+        // 2. suruh si kurir ambil SEMUA data dari tabel bahasa
+        $semua_kata = $bahasaModel->findAll();
+
+        // 3. bungkus datanya ke dalam variabel 'data' buat dikirim ke View
+        $data = [
+            'title' => 'KELEKAK.ID - Beranda', // Judul halaman
+            'kamus' => $semua_kata             // Isi datanya
+        ];
+
+        // 4. tampilin halaman view bernama 'v_beranda' sambil bawa bungkusan datanya
+        return view('v_beranda', $data);
     }
 }
